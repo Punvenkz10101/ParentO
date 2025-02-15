a378b3b403587c90bb6c0b0b688cc905bad95f50
Bharath P <bharathkkl907@gmail.com>
2025-02-16 00:27:54 +0530

    changed port

--- Backend/server.js
+++ Backend/server.js
@@ -19,5 +19,5 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/parent', parentAuthRoutes);
 app.use('/api/teacher', teacherAuthRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
