8fef8a9bbc0ae910d6a547c38ae115fed18f1c74
Bharath P <bharathkkl907@gmail.com>
2025-02-20 20:52:09 +0530

    small changes

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const parentAuthRoutes = require('./routes/authParent');
 const teacherAuthRoutes = require('./routes/authTeacher');
 const parentProfileRoutes = require('./routes/ParentProfileDataRoute');
+const teacherProfileRoutes = require('./routes/TeacherProfileDataRoutes');
 
 const app = express();
 
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/parent', parentAuthRoutes);
 app.use('/api/teacher', teacherAuthRoutes);
 app.use('/api', parentProfileRoutes);
+app.use('/api', teacherProfileRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
