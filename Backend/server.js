76605b6ad3a693fb5b40bf4413913c1b401cfed6
Bharath P <bharathkkl907@gmail.com>
2025-02-23 17:56:02 +0530

    port change

--- Backend/server.js
+++ Backend/server.js
@@ -95,7 +95,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
